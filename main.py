b7a220e4deafd03d894d9a2ceace5e9a5f6dfcbb
AnRyz <36033764+AnRyz@users.noreply.github.com>
2018-02-22 23:20:38 +0000

    First commit

---
+++ main.py
@@ -0,0 +1,13 @@
+'''
+Created on 22 Feb 2018
+
+@author: Anna
+'''
+import platform
+
+def main():
+    print(platform.platform())
+    return
+
+if __name__ == '__main__':
+    pass
--- setup.py
+++ setup.py
@@ -1,25 +1,18 @@
-# -*- coding: utf-8 -*-
+'''
+Created on 22 Feb 2018
 
-# Learn more: https://github.com/kennethreitz/setup.py
+@author: Anna
+'''
+from setuptools import setup
 
-from setuptools import setup, find_packages
-
-
-with open('README.rst') as f:
-    readme = f.read()
-
-with open('LICENSE') as f:
-    license = f.read()
-
-setup(
-    name='sample',
-    version='0.1.0',
-    description='Sample package for Python-Guide.org',
-    long_description=readme,
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
-    url='https://github.com/kennethreitz/samplemod',
-    license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
-)
-
+setup(name="systeminfo",
+      version="0.1",
+      description="Basic system information for COMP30670",
+      author="Anna Ryzova",
+      author_email="anna.ryzova@ucdconnect.ie",
+      license="GPL3",
+      packages=['systeminfo'],
+      entry_points={
+          'console_scripts':['comp30670_systeminfo=systeminfo.main:main']
+          }
+ )
